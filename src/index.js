c80defef24682a0ae94e12c4cebe5932fb3465ed
dooferette <dooferette@users.noreply.github.com>
2020-11-24 17:08:13 +0000

    Adding Forecast API to JS

--- src/index.js
+++ src/index.js
@@ -32,6 +32,19 @@ function formatTime(){
   return currentTime
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 
 let weekDay = formatDate();
 let weekDayDisplay = document.querySelector("h2#day");
@@ -40,6 +53,7 @@ weekDayDisplay.innerHTML = weekDay;
 let timeNow = formatTime();
 let timeNowDisplay = document.querySelector("h2#time");
 timeNowDisplay.innerHTML = timeNow;
+
 
 // Celcius and Fahrenheit Conversion
 
@@ -84,10 +98,33 @@ function displayWeather(response){
   celsiusTemp = response.data.main.temp;
 }
 
+function displayForecast(response){
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2 ml-1 md-1">
+            <p class="forecast-grid-time">${formatHours(forecast.dt * 1000)}</p3>
+              <br />
+              <img src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png" alt="rain" class="main-icon" id="icon">
+              <p class="forecast-grid-temperature">${Math.round(forecast.main.temp)}°C</p3>
+              </div>
+              `
+  }
+}
+
 function searchCity(city) {
   let apiKey = "f8da540cbadec070f67f3f54a6f1afb1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+
+  apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function showCity(event){
